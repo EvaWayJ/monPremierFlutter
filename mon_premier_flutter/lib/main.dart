cb51b4818ad3674f76fadd0bd932314ab65d26b4
EvaWayJ <ezzeribieva@yahoo.fr>
2020-04-13 17:44:27 +0200

    Création d'un Scaffold - Le column

--- mon_premier_flutter/lib/main.dart
+++ mon_premier_flutter/lib/main.dart
@@ -32,35 +32,42 @@ class _Home extends State<Home>{
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('salut'),
-        leading: new Icon(Icons.account_circle),
-        actions: <Widget>[
-          new Icon(Icons.access_alarm, size: 50),
-          new Icon(Icons.golf_course, color: Colors.indigo.shade800,size: 50,),
-          new Icon(Icons.directions_bike),
-        ],
-        elevation: 10.0,
-        centerTitle: true,
-      ),
-      body: new Container(
-        color: Colors.indigo,
-        //height: MediaQuery.of(context).size.height/2,
-        //width: MediaQuery.of(context).size.width /3,
-        margin: EdgeInsets.only(top: 20.0, bottom: 15.0),
-        child: new Center(
-          child: new Text(
-              "Apprentissage des widgets avec les cours de codabee.com j'adore ca",
-                  textAlign: TextAlign.center,
-            textScaleFactor: 2.0,
-            style: new TextStyle(
-              color: Colors.white,
-              fontSize: 20.0,
-              fontStyle: FontStyle.italic
-            ),
-            maxLines: 2,
+        appBar: new AppBar(
+          title: new Text('salut'),
+          leading: new Icon(Icons.account_circle),
+          actions: <Widget>[
+            new Icon(Icons.access_alarm, size: 50),
+            new Icon(Icons.golf_course, color: Colors.indigo.shade800,size: 50,),
+            new Icon(Icons.directions_bike),
+          ],
+          elevation: 10.0,
+          centerTitle: true,
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new Text("Salut les codeur",style: new TextStyle(
+                  color: Colors.grey,
+                  fontSize: 30.0,
+                  fontStyle: FontStyle.italic
+              ),
+              ),
+              new Card(
+                child: new Container(
+                  width: MediaQuery.of(context).size.width /1.5,
+                  height: 200.0,
+                  child: new Image.asset('',
+                      fit: BoxFit.cover
+                  ),
+                ),
+              ),
+              new Container(
+                height: 75.0,
+                color: Colors.red,
+              )
+            ],
           ),
-        ),
-        ),
+        )
     );
   }}
